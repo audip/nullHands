910be01f8bfbb18e5b540e78df3b5898b011c076
Wilson Ding <shodai100@gmail.com>
2016-09-10 04:06:50 -0400

    Backup

---
+++ Pods/Target Support Files/Pods-nullHands/Pods-nullHands-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_nullHands : NSObject
+@end
+@implementation PodsDummy_Pods_nullHands
+@end
--- nullHands/ViewController.swift
+++ nullHands/ViewController.swift
@@ -7,19 +7,29 @@
 //
 
 import UIKit
+import Firebase
+import CoreMotion
 
 class ViewController: UIViewController {
-
+    
+    var ref: FIRDatabaseReference!
+    
+    var manager: CMMotionManager!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.ref = FIRDatabase.database().reference()
+            
+        self.ref.child("gyro").setValue(["x": 0,
+                                         "y": 0])
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
-
