ec1cdcf193cd7c3183229ce6fff7827c0fb436e4
Wilson Ding <shodai100@gmail.com>
2016-09-10 09:32:05 -0400

    Working Gyroscope

--- nullHands/ViewController.swift
+++ nullHands/ViewController.swift
@@ -14,17 +14,27 @@ class ViewController: UIViewController {
     
     var ref: FIRDatabaseReference!
     
-    var manager: CMMotionManager!
+    var manager: CMMotionManager = CMMotionManager()
+    var attitude: CMAttitude = CMAttitude()
+    var motion: CMDeviceMotion = CMDeviceMotion()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
         self.ref = FIRDatabase.database().reference()
-            
-        self.ref.child("gyro").setValue(["x": 0,
-                                         "y": 0])
-    }
+
+        manager.deviceMotionUpdateInterval = 0.01
+        manager.startDeviceMotionUpdates(to: OperationQueue.current!, withHandler:{
+            deviceManager, error in
+            self.motion = self.manager.deviceMotion!
+            self.attitude = self.motion.attitude
+            self.ref.child("gyro").setValue(["x": self.attitude.yaw,
+                                             "y": self.attitude.pitch,
+                                             "z": self.attitude.roll])
+        })
+        
+        print(manager.isDeviceMotionActive)
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
